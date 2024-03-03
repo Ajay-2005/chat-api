f54fd436660b61987da7962d62e1d684ad802fa6
Ajay <aji478514@gmail.com>
2024-03-03 13:13:06 +0530

    clear text logging of sensitive information

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -233,7 +233,7 @@ module.exports = {
 
             const user = await collection.usercollection.findOne({ resettoken: token });
             console.log(user);
-            console.log(newpassword);
+            
 
             if (!user) {
                 return res.status(400).json({ error: 'Invalid or expired token' });
